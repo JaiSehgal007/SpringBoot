a15ecb498fb7555297198429ad25cfcbe6d9a3f5
JaiSehgal007 <jaisehgal511@gmail.com>
2024-12-18 00:06:42 +0530

    Created a basic setup of SpringBoot and built API endpoint

---
+++ myFirstProject/myFirstProject/src/main/java/com/codecats/myFirstProject/myClass.java
@@ -0,0 +1,14 @@
+package com.codecats.myFirstProject;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class myClass {
+
+    @GetMapping("abc")
+    public String sayHello() {
+        return "Hello World";
+    }
+}
