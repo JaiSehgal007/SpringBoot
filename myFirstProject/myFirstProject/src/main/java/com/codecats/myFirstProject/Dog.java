94b3f4a225de298f5392b7b6cbff43b09d93e835
JaiSehgal007 <jaisehgal511@gmail.com>
2024-12-20 00:45:12 +0530

    Understanding the internal architecture

---
+++ myFirstProject/myFirstProject/src/main/java/com/codecats/myFirstProject/Dog.java
@@ -0,0 +1,10 @@
+package com.codecats.myFirstProject;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog {
+    public String fun(){
+        return "dog";
+    }
+}
--- myFirstProject/myFirstProject/src/main/java/com/codecats/myFirstProject/myClass.java
+++ myFirstProject/myFirstProject/src/main/java/com/codecats/myFirstProject/myClass.java
@@ -1,14 +1,24 @@
 package com.codecats.myFirstProject;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class myClass {
 
+    @Autowired // this is known as dependency injection ( myClass is dependant on Dog)
+    private Dog dog; // we need not write new (this is field injection)
+    // we asked for a object (bean)
+
     @GetMapping("abc")
     public String sayHello() {
-        return "Hello World";
+        return dog.fun();
     }
 }
+
+/*
+* this @RestController also make a bean only
+* but is is a something more than @Component
+* */
